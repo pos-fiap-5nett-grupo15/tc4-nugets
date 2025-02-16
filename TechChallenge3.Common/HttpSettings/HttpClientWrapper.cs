3d23ee9000363303c5f0897a07d2d69e9c776ea7
dev-guilhCASTRO <guilherme.cbpereira@gmail.com>
2025-02-16 14:38:34 -0300

    Added http client

---
+++ TechChallenge3.Common/HttpSettings/HttpClientWrapper.cs
@@ -0,0 +1,35 @@
+﻿using System.Net;
+
+namespace TechChallenge3.Common.HttpSettings
+{
+    public sealed class HttpClientWrapper : HttpClient, IHttpClient
+    {
+        public HttpClientWrapper(int hours, int minutes, int seconds)
+        {
+            this.Timeout = new TimeSpan(hours, minutes, seconds);
+        }
+
+        public async Task<Tuple<string, HttpStatusCode?>> PostAndGetRawResponseAsync(string requestUri, HttpContent content, Dictionary<string, string> headers = null)
+        {
+            if (headers != null)
+            {
+                foreach (KeyValuePair<string, string> header in headers)
+                {
+                    this.DefaultRequestHeaders.TryAddWithoutValidation(header.Key, header.Value);
+                }
+            }
+
+            HttpResponseMessage httpResponse = await this.PostAsync(requestUri, content);
+
+            // Error: Could not communicate with the endpoint.
+            if (httpResponse == null)
+            {
+                return default;
+            }
+
+            return new Tuple<string, Nullable<HttpStatusCode>>(
+                await httpResponse.Content.ReadAsStringAsync(),
+                httpResponse.StatusCode);
+        }
+    }
+}
---
+++ TechChallenge3.Common/HttpSettings/IHttpClient.cs
@@ -0,0 +1,12 @@
+﻿using System.Net;
+
+namespace TechChallenge3.Common.HttpSettings
+{
+    public interface IHttpClient
+    {
+        Task<Tuple<string, HttpStatusCode?>> PostAndGetRawResponseAsync(
+            string requestUri,
+            HttpContent content,
+            Dictionary<string, string> headers = default);
+    }
+}
